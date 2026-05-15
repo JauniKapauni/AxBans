582e6888a8f02360a5d00057011a20ec1d838065
JauniKapauni <riches-goods.54@icloud.com>
2026-05-15 12:29:12 +0200

    feat: add DatabaseManager

--- src/main/java/de/jaunikapauni/axbans/AxBans.java
+++ src/main/java/de/jaunikapauni/axbans/AxBans.java
@@ -1,12 +1,28 @@
 package de.jaunikapauni.axbans;
 
+import de.jaunikapauni.axbans.manager.DatabaseManager;
+import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class AxBans extends JavaPlugin {
+    DatabaseManager databaseManager;
+    public DatabaseManager getDatabaseManager(){
+        return databaseManager;
+    }
 
     @Override
     public void onEnable() {
         // Plugin startup logic
+        databaseManager = new DatabaseManager(this);
+        try {
+            if (databaseManager.initDatabaseTable1() == false) {
+                getLogger().severe("Error creating table!");
+                Bukkit.getServer().shutdown();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        getLogger().info("DB connection successfully established!");
     }
 
     @Override
